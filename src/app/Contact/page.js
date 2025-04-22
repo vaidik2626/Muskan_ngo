b1c05501bb209bed478da165d7bc095146fc24bd
vaidik2626 <21it047@charusat.edu.in>
2025-04-22 23:08:22 +0530

    fix: correct HTML entity for apostrophe in feedback text

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -25,7 +25,7 @@ export default function ContactSection() {
   <div className="container px-5 py-24 mx-auto flex">
     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
       <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p className="leading-relaxed mb-5 text-gray-600">Want to get Touch ? We'd love to hear from you.</p>
+      <p className="leading-relaxed mb-5 text-gray-600">Want to get Touch ? We&apos;d love to hear from you.</p>
       <div className="relative mb-4">
         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
         <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
