36b8157ffc9890194823e428caba0c9230804af1
vaidik2626 <99650583+vaidik2626@users.noreply.github.com>
2025-05-12 21:49:12 +0530

    Update page.js

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -56,7 +56,7 @@ const Aboutus = () => {
       <ValuesCarousel />
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <Image
-          src="/Assets/Aboutus/ourmission.png"
+          src="/Assets/Aboutus/OurMission.png"
           alt=""
           fill
           className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"
