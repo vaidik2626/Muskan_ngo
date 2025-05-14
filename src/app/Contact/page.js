1be23e47effdf05e2db57bd1bfa8ee260a0ae075
vaidik2626 <99650583+vaidik2626@users.noreply.github.com>
2025-05-14 22:08:00 +0530

    Update page.js

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -31,7 +31,7 @@ export default function ContactCorporateSection() {
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-            <p className="leading-relaxed mb-5 text-gray-600">Want to get Touch? We'd love to hear from you.</p>
+            <p className="leading-relaxed mb-5 text-gray-600">Want to get Touch? We&apos;d love to hear from you.</p>
             <div className="relative mb-4">
               <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
               <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3" />
@@ -41,7 +41,7 @@ export default function ContactCorporateSection() {
               <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none"></textarea>
             </div>
             <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
-            <p className="text-xs text-gray-500 mt-3">Here's how you can reach us.</p>
+            <p className="text-xs text-gray-500 mt-3">Here&apos;s how you can reach us.</p>
           </div>
         </div>
       </section>
@@ -132,7 +132,7 @@ export default function ContactCorporateSection() {
       {/* Contact CTA Bar */}
       <div className="bg-cyan-700 text-white px-4 py-6 md:px-16 lg:px-32 text-center md:text-left">
         <p className="mb-4 font-semibold text-lg">
-          We're always open to building customized partnership models based on your goals.
+          We&apos;re always open to building customized partnership models based on your goals.
         </p>
         <div className="grid md:grid-cols-3 gap-4 text-sm">
           <p>
